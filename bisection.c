3d407bcdf4ededbd78994c0b9c54cdfd8457de5f
Arthurit0 <arthur.jose27@gmail.com>
2022-03-31 22:52:14 -0300

    Finalizando primeiro exercício em aula

--- bisection.c
+++ bisection.c
@@ -1,21 +1,31 @@
 #include <stdio.h>
+#include <math.h>
 
-void bisection(double *(f)(double), double a, double b, int n) {
+void bisection(double (*f)(double), double a, double b, int n) {
     if (f(a) * f(b) >= 0){
         printf("Nao eh possivel usar Bolzano para garantir a existencia de uma raiz em [%f, %f]", a, b);
     }else{
         for(int i=0; i<n; i++){
             double m=0.5*(a+b);
-            printf
+            printf("x_%d = %.16f\n",i+1,m);
+            if(f(m) == 0){
+                printf("Você encontrou uma raíz r = %.16f", m);
+            }else{
+                f(a)*f(m) < 0 ? (b = m) : (a = m);
+            }
         }
     }
 }
 
 int main(){
     // Exemplo 1: f(x) = x³-2, [0,2]
-    double f(double x){
-        return x*x*x-2.0;
+    double f(double x) {
+        return pow(x,3)-2.0;
     }
+
+    double a = 0;
+    double b = 2;
+    double n = 10;
 
     // Exemplo 2: Crescimento populacional
     double P(double x){
@@ -26,6 +36,9 @@ int main(){
     double b1 = 2;
     int n1 = 20;
 
-    // bisection(f, a, b, n);
+    printf("\n1. Exemplo da função x³-2, [0,2]:\n\n");
+    bisection(f, a, b, n);
+    printf ("\n2. Problema do crescimento populacional:\n\n");
+    bisection (P, a1, b1, n1);
 
 }
