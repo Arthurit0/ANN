4351158f464551106ffa7f16f53d3a9b580d5273
Arthur José <57883830+Arthurit0@users.noreply.github.com>
2022-04-24 13:53:22 -0300

    Refiz algoritmo de encontrar Polin. Interpolador

--- inter.py
+++ inter.py
@@ -1,40 +1,49 @@
 import numpy as np
 
-    #Exemplos
-    x = [1, 2, 3]
-    y = [1, 4, 1]
+# Define os coeficientes do polinômio
+def poly(x,y):
+    n = len(x) - 1
+    A = []
+    B = y
+    for xi in x:
+        row = [1]
+        for j in range(1, n + 1):
+            row.append(xi ** j)
+        A.append(row)
+    return np.linalg.solve(A,B)
 
+
+def func_poly(x, coeffs):
+    first = coeffs[0]
+    return first + sum([ai * x ** j for j, ai in enumerate(coeffs[1:],1)])    
+
+if __name__ == '__main__':
+
+    # Exemplo 1
+    # x = [1, 2, 3]
+    # y = [1, 4, 1]
+
+    # Exemplo 2
+    x = [0, np.pi / 2, np.pi]
+    y = [0,1,0]
 
     coeffs = poly(x,y)
     print(coeffs)
-
     def p(x):
         return func_poly(x, coeffs)
 
-    #Vsualização
-
+    # Visualização
     import matplotlib.pylab as plt
+
     plt.scatter(x,y)
 
-    t = np.linspace(min(x), max(x), 200)
-    pt = [p(ti)]
+    t = np.linspace(min(x),max(x), 200)
+    pt = p(t)
 
+    # Função seno, Exemplo 2
+    st = np.sin(t)
 
-    def poly(x, y):
-        n = len(x) - 1
-        A = []
-        B = y
-        for xi in x:
-            row = [1]
-            for j in range(1, n + 1):
-                row.append(xi ** j)
-            A.append(row)
-        return np.linalg.solve(A,y)
+    plt.plot(t,pt)
+    plt.plot(t, st)
 
-    if __name__ == '__main__':
-        coeffs = poly(x,y)
-        print(coeffs)
-
-    def func_poly(x, coeffs):
-        first = coeffs[0]
-        return first + sum([ai * x ** j for j, ai in enumerate(coeffs[1:],1)])
+    plt.savefig('interp.png')
