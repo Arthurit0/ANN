79e96a9ad96bfac4dd121608e56d1819238e75b3
Arthurit0 <57883830+Arthurit0@users.noreply.github.com>
2022-05-07 18:47:29 -0300

    Questão das molas feita

--- 3 molas/seidel para questão das molas.c
+++ 3 molas/seidel para questão das molas.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <math.h>
 
 #define ROWS 3
 
@@ -29,32 +28,17 @@ void seidel(double A[ROWS][ROWS], double B[ROWS], double est[ROWS], int n)
 
 int main(int argc, char const *argv[])
 {
-    double m1 = 176,
-           m2 = 80,
-           m3 = 74,
-           u1 = 0.16,
-           u2 = 0.44,
-           u3 = 0.47,
-           g = 9.81,
-           ang = 0.733038, // 42 graus em rad
-        F1h = m1 * g * (sin(ang)),
-           F2h = m2 * g * (sin(ang)),
-           F3h = m3 * g * (sin(ang)),
-           F1v = m1 * g * (cos(ang)),
-           F2v = m2 * g * (cos(ang)),
-           F3v = m3 * g * (cos(ang)),
-           f1 = u1 * F1v,
-           f2 = u2 * F2v,
-           f3 = u3 * F3v;
-
-    printf("sen %.7lf cos %.7lf", F1h, F1v);
-
+    double k1 = 58.11,
+           k2 = 65.57,
+           k3 = 79.14,
+           m1 = 3.12,
+           m2 = 2.3,
+           m3 = 2.48,
+           g = 9.81;
     // Coeficientes do Sistema em que aplicaremos a técnica
-    double A[ROWS][ROWS] = {{m3, 1, 0},
-                            {m2, -1, 1},
-                            {m3, 0, -1}};
+    double A[ROWS][ROWS] = {{k1 + k2, -k2, 0}, {-k2, k2 + k3, -k3}, {0, -k3, k3}};
     // Termos independentes
-    double B[ROWS] = {F1h - f1, F2h - f2, F3h - f3};
+    double B[ROWS] = {m1 * g, m2 * g, m3 * g};
     // Estimativa de soluções
     double est_ini[ROWS] = {50, 50, 50};
     // Iterações
