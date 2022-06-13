be0b387e29dd5016084d8d61c58ae627255f8f5e
Arthur José <57883830+Arthurit0@users.noreply.github.com>
2022-06-13 09:42:27 -0300

    Método da Quadratura Gaussiana

---
+++ Assuntos da Prova 2/quadratura_gaussiana.py
@@ -0,0 +1,21 @@
+import math
+
+def p(x):
+    return 1-3*x+4*x**2-2*x**3
+
+r = math.sqrt(3) / 3
+
+aprox = p(-r)+p(r)
+
+exato = 14/3
+
+print(f'{aprox - exato = }')
+
+def f(x):
+    return math.exp(-x**2)
+
+aprox = f(-r)+f(r)
+
+exato = 1.14
+
+print(f'{aprox - exato = }')
